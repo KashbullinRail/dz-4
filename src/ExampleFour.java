8f408b4e76a16f4790977236b0e9093abd71d56f
KashbullinRail <kashbullinrail@gmail.com>
2022-04-19 18:11:15 +0300

    Мое домашнее задание №4 по разделу "Циклы"

--- src/ExampleFour.java
+++ src/ExampleFour.java
@@ -1,16 +1,17 @@
 /**
  * SkyPro. My code dz#4
  *
- * @author ?????
+ * @author RAIL
  * @version dated Apr 17, 2022
  */
 
 public class ExampleFour {
     public static void main(String[] args) {
 
-        // Задание 1
-        int i = 1;
         System.out.println();
+        // Задание 1
+        System.out.println("Задание 1");
+        int i = 1;
         while (i < 11) {
             System.out.print(i + " ");
             i++;
@@ -22,6 +23,7 @@ public class ExampleFour {
 
         // Задание 2
         System.out.println("\n");
+        System.out.println("Задание 2");
         int d = 3, a = 0;
         while ( d <= 31) {
             if (a % 7 == 0) {
@@ -33,16 +35,18 @@ public class ExampleFour {
 
         // Задание 3
         System.out.println();
+        System.out.println("Задание 3");
         int year = 2021; // Для соответствия дат указанных в ДЗ необходимо ввести: 1975 or 2054.
+        int z = year;
         for (int b = 0; b < 200; b++) {
            if (b %79 == 0) {
-               System.out.println(year);
-           } year--;
-        } year = 2021;
+               System.out.println(z);
+           } z--;
+        } z = year;
         for (int c = 0; c < 100; c++) {
-            if (c %79 == 0 & year != 2021) {
-                System.out.println(year);
-            } year++;
+            if (c %79 == 0 & z != year) {
+                System.out.println(z);
+            } z++;
         }
     }
 }
